e532d55e09d25423aa3aac657c2601a85c566392
Heisenberg <heisenberg@buaa.edu.cn>
2025-06-20 00:29:38 +0800

    -

--- common/src/main/java/io/github/heisenberguwu/myrocketmq/common/utils/NetworkUtil.java
+++ common/src/main/java/io/github/heisenberguwu/myrocketmq/common/utils/NetworkUtil.java
@@ -4,15 +4,31 @@ import io.github.heisenberguwu.myrocketmq.common.constant.LoggerName;
 import org.apache.rocketmq.logging.org.slf4j.Logger;
 import org.apache.rocketmq.logging.org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.nio.channels.Selector;
 import java.nio.channels.spi.SelectorProvider;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 
 public class NetworkUtil {
+    /**
+     * | 应用方向           | 说明                                                |
+     * | -------------- | ------------------------------------------------- |
+     * | **网络调试和监控**    | 查看有哪些网卡启用、IP 配置，判断网络连接是否正常。                       |
+     * | **服务 IP 绑定**   | 自动选择可用的网卡 IP（如绑定在 `eth0` 而不是 `127.0.0.1`）来对外提供服务。 |
+     * | **多网卡负载分流**    | 在多个物理网卡（或 VLAN）上做负载均衡或故障转移。                       |
+     * | **分布式服务注册与发现** | 自动选择合适 IP 地址注册到注册中心（如 Nacos、Consul、Zookeeper）。    |
+     * | **系统信息采集**     | 采集主机硬件信息，用于运维、CMDB、资产管理。                          |
+     * | **唯一机器标识**     | 使用某个物理网卡的 MAC 地址作为机器 ID。                          |
+     * | **防火墙、安全策略**   | 判断数据包是否来自可信网卡、是否跨网段。                              |
+     * | **网络虚拟化/容器网络** | 管理容器或虚拟机的虚拟网卡，分析网络拓扑。                             |
+     */
     public static final String OS_NAME = System.getProperty("os.name"); // 通过JVM参数获取操作系统
 
     private static final Logger log = LoggerFactory.getLogger(LoggerName.COMMON_LOGGER_NAME);
@@ -42,6 +58,7 @@ public class NetworkUtil {
     /**
      * SelectionKey 选择器 ** Selector ** 的获取方法
      * Selector - 监听事件 - SelectionKey - 绑定Channel - Channel - 封装 - Socket
+     *
      * @return
      * @throws IOException
      */
@@ -91,8 +108,40 @@ public class NetworkUtil {
         return result;
     }
 
-    public static List<InetAddress getLocalInetAddressList() throws SocketException
-    {
+    // 获取当前主机上所有“正常启用的物理网卡”所绑定的合法 IP 地址（IPv4 和 IPv6）列表。
+    public static List<InetAddress> getLocalInetAddressList() throws SocketException {
+        Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
+        ArrayList<InetAddress> inetAddressList = new ArrayList<>();
+        /*
+        跳过以下网络接口：
 
+            桥接接口（isBridge(nif)，这个是你自定义的判断函数）
+
+            虚拟接口（nif.isVirtual()）
+
+            点对点接口，如 PPP（拨号上网等，nif.isPointToPoint()）
+
+            非运行状态（!nif.isUp()）
+         */
+        while (enumeration.hasMoreElements()) {
+            final NetworkInterface nif = enumeration.nextElement();
+            if (is)
+        }
+    }
+
+    private static boolean isBridge(NetworkInterface networkInterface) {
+        // 在 Linux /sys/class/net/xxx/bridge 下面有文件描述符，那么就是桥接网卡
+        // - ls /sys/class/net/docker0/bridge    # 存在，说明 docker0 是 bridge
+        // - ls /sys/class/net/eth0/bridge       # 不存在，说明 eth0 不是 bridge
+        try {
+            if (isLinuxPlatform()) {
+                String interfaceName = networkInterface.getName();
+                File file = new File("/sys/class/net/" + interfaceName + "/bridge");
+                return file.exists();
+            }
+        } catch (SecurityException e) {
+            // Ignore
+        }
+        return false;
     }
 }
