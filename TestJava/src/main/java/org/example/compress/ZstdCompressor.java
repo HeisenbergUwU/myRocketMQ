558042bc5b49686df798697d99f35f4bf7274eaa
Heisenberg <heisenberg@buaa.edu.cn>
2025-06-27 23:28:21 +0800

    -

--- TestJava/src/main/java/org/example/compress/ZstdCompressor.java
+++ TestJava/src/main/java/org/example/compress/ZstdCompressor.java
@@ -1,38 +1,60 @@
 package org.example.compress;
 
+import com.github.luben.zstd.ZstdInputStream;
+import com.github.luben.zstd.ZstdOutputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-public class ZstdCompressor implements ZstdCompressor.Compressor {
-    @Override
+public class ZstdCompressor {
     public byte[] compress(byte[] src, int level) throws IOException {
-        return new byte[0];
+        byte[] result = src;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(src.length);
+        ZstdOutputStream outputStream = new ZstdOutputStream(byteArrayOutputStream, level);
+        try{
+            outputStream.write(src);
+            outputStream.flush();
+            outputStream.close();
+            result = byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                byteArrayOutputStream.close();
+            } catch (IOException ignored) {
+            }
+        }
+        return result;
     }
 
-    @Override
     public byte[] decompress(byte[] src) throws IOException {
-        return new byte[0];
+        byte[] result = src;
+        byte[] uncompressData = new byte[src.length];
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(src); // 输入流
+        ZstdInputStream zstdInputStream = new ZstdInputStream(byteArrayInputStream); // 输入压缩工具流
+        ByteArrayOutputStream resultOutputStream = new ByteArrayOutputStream(src.length); // 输出工具流
+        try {
+            while (true) {
+                int len = zstdInputStream.read(uncompressData, 0, uncompressData.length);
+                if (len <= 0) {
+                    break;
+                }
+                resultOutputStream.write(uncompressData, 0, len);
+            }
+            resultOutputStream.flush();
+            resultOutputStream.close();
+            result = resultOutputStream.toByteArray();
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            try {
+                zstdInputStream.close();
+                byteArrayInputStream.close();
+            } catch (IOException e) {
+            }
+        }
+
+        return result;
     }
-
-    public interface Compressor {
-
-        /**
-         * Compress message by different compressor.
-         *
-         * @param src   bytes ready to compress
-         * @param level compression level used to balance compression rate and time consumption
-         * @return compressed byte data
-         * @throws IOException
-         */
-        byte[] compress(byte[] src, int level) throws IOException;
-
-        /**
-         * Decompress message by different compressor.
-         *
-         * @param src bytes ready to decompress
-         * @return decompressed byte data
-         * @throws IOException
-         */
-        byte[] decompress(byte[] src) throws IOException;
-    }
-
 }
--- TestJava/src/main/java/org/example/testJsonDeserializer.java
+++ TestJava/src/main/java/org/example/testJsonDeserializer.java
@@ -11,24 +11,6 @@ import java.util.Map;
 
 public class testJsonDeserializer {
 
-    // 自定义继承带泛型父类的Map
-    static class MyStringObjectMap extends java.util.HashMap<String, Object> {
-    }
-
-    static class MyLongFooMap extends java.util.HashMap<Long, Foo> {
-    }
-
-    // 简单的 Foo 类用于测试 valueType
-    static class Foo {
-        public int id;
-        public String name;
-
-        @Override
-        public String toString() {
-            return "Foo{id=" + id + ", name='" + name + "'}";
-        }
-    }
-
 
     public static void main(String[] args) {
         // 1. 准备 JSON
@@ -36,10 +18,15 @@ public class testJsonDeserializer {
 
         // 2. 注册自定义反序列化器
         ParserConfig config = new ParserConfig();
-        config.putDeserializer(HashMap.class,GenericMapSuperclassDeserializer.INSTANCE);
-
+        config.putDeserializer(HashMap.class,GenericMapSuperclassDeserializer.INSTANCE); // 单词配置
+        // 把这段注册逻辑放在应用启动入口（如 main 方法或 Spring Boot 的 @PostConstruct 中），即可实现全局生效。
+        ParserConfig.getGlobalInstance()
+                .putDeserializer(HashMap.class, GenericMapSuperclassDeserializer.INSTANCE);
         // 3. 调用 fastJson 解析
-        HashMap result = JSON.parseObject(json, HashMap.class, config);
-        System.out.println(result);
+//        HashMap result = JSON.parseObject(json, HashMap.class, config);
+//        System.out.println(result);
+
+        HashMap s = JSON.parseObject(json, HashMap.class);
+        System.out.println(s);
     }
 }
