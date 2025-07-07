34d2e193fcfd2d376339713877f82b32f3255bb5
Heisenberg <heisenberg@buaa.edu.cn>
2025-07-08 01:09:03 +0800

    -

--- common/src/test/java/org/apache/rocketmq/common/AppTest.java
+++ common/src/test/java/org/apache/rocketmq/common/AppTest.java
@@ -2,7 +2,9 @@ package org.apache.rocketmq.common;
 
 import org.apache.rocketmq.common.attribute.AttributeParser;
 import junit.framework.TestCase;
+import org.apache.rocketmq.common.config.ConfigRocksDBStorage;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 /**
@@ -19,4 +21,25 @@ public class AppTest
         Map<String, String> stringStringMap = AttributeParser.parseToMap(targets);
         System.out.println(AttributeParser.parseToString(stringStringMap));
     }
+
+    public void testDB() throws Exception {
+        ConfigRocksDBStorage storage = new ConfigRocksDBStorage("./test");
+        storage.start();
+        // 加载 RocksDB
+        // 写入数据
+        String key = "exampleKey";
+        String value = "exampleValue";
+        storage.put(key.getBytes(StandardCharsets.UTF_8), key.length(), value.getBytes(StandardCharsets.UTF_8));
+        System.out.println("Put data: key = " + key + ", value = " + value);
+
+
+        // 读取数据
+        byte[] valueBytes = storage.get(key.getBytes(StandardCharsets.UTF_8));
+        if (valueBytes != null) {
+            System.out.println("Get data: key = " + key + ", value = " + new String(valueBytes, StandardCharsets.UTF_8));
+        } else {
+            System.out.println("No value found for key: " + key);
+        }
+    }
+
 }
